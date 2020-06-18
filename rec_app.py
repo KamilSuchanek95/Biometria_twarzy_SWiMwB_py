57a00beb967b365313a5b2f16986bdfe1d02f67d
Kamil Suchanek <kamilsuchanek95@gmail.com>
2020-06-18 11:38:28 +0200

    naprawiono logikę przycisków, wszystkie trzy działają
    Jednak jest to nieelegancko zaimplementowane. Pozycja przycisków została poprawiona, jednak rozmieszczanie elementów w TK jest dość niezgrabne...

--- rec_app.py
+++ rec_app.py
@@ -1,7 +1,7 @@
 import SWiMwB as s
 import tkinter as tki
 import cv2
-import threading
+import datetime
 from PIL import Image, ImageTk
 
 
@@ -13,62 +13,70 @@ class RecognitionApp:
         self.identities = {}
         self.toggle_camera = 1
         self.webcam = cv2.VideoCapture(0)
+        self.off = 0
+        self.current_image = []
 
         # ustawienia okna
         self.window = tki.Tk()
         self.window.wm_title("Prototyp aplikacji systemu biometrycznego")
-        self.window.config(background="#FF44FF")
+        self.window.config(background="#0048FF")
         self.window.protocol("WM_DELETE_WINDOW", self.onClose)
 
         self.frame = tki.Frame(self.window, width=1000, height=500)
         self.frame.grid(row=0,column=0,columnspan=10,rowspan=10)
 
         self.camera_label = tki.Label(self.window)
-        self.camera_label.grid(row=1,column=1,columnspan=9,rowspan=9)
+        self.camera_label.grid(row=2,column=1,columnspan=3,rowspan=3, sticky='nsew')
 
         # elementy GUI
-        button_camera_on_of = tki.Button(self.window, text="Turn on the camera",
-                                         command=self.buttonCamera)
-        button_camera_on_of.grid(row=1, column=6, columnspan=2, rowspan=2, sticky='nsew')
-        button_camera_on_of = tki.Button(self.window, text="Turn off the camera",
-                                         command=self.turnOffCamera)
-        button_camera_on_of.grid(row=4, column=6, columnspan=2, rowspan=2, sticky='nsew')
-        button_get_image = tki.Button(self.window, text="Get the image",
-                                         command=self.getImage)
+        button_camera_on = tki.Button(self.window, text="Turn on the camera", command=self.buttonCamera)
+        button_camera_on.grid(row=1, column=6, columnspan=2, rowspan=2, sticky='nsew')
+        button_camera_off = tki.Button(self.window, text="Turn off the camera", command=self.turnOffCamera)
+        button_camera_off.grid(row=4, column=6, columnspan=2, rowspan=2, sticky='nsew')
+        button_get_image = tki.Button(self.window, text="Get the image", command=self.getImage)
         button_get_image.grid(row=7, column=6, columnspan=2, rowspan=2, sticky='nsew')
 
 
     def turnOnCamera(self):
-        self.webcam = cv2.VideoCapture(0)
-        check, image = self.webcam.read()
-        if check:
-            image = Image.fromarray(image)
-            image = ImageTk.PhotoImage(image)
-            self.camera_label.image = image
-            self.camera_label.config(image=image)
-        self.window.after(30, self.turnOnCamera)
-
-
-    def turnOffCamera(self):
-        if self.turnOnCamera is not None:
-            self.window.after_cancel(self.turnOnCamera)
-            # self.turnOnCamera = None
-        self.webcam.release()
-        self.toggle_camera = self.toggle_camera * (-1)
+        if self.off:
+            self.webcam = cv2.VideoCapture(0)
+            self.off = 0
+        else:
+            check, self.current_image = self.webcam.read()
+            if check:
+                image = Image.fromarray(self.current_image)
+                image = ImageTk.PhotoImage(image)
+                self.camera_label.image = image
+                self.camera_label.config(image=image)
+            self.window.after(30, self.turnOnCamera)
 
 
     def buttonCamera(self):
-        if self.toggle_camera>0:
+        if self.toggle_camera > 0:
             self.toggle_camera = self.toggle_camera * (-1)
             self.turnOnCamera()
 
 
-    def getImage(self):
-        if self.turnOnCamera is not None:
+    def turnOffCamera(self):
+        # if camera is turned on
+        if self.toggle_camera < 0:
+            self.toggle_camera = self.toggle_camera * (-1)
+            # turn off loop
             self.window.after_cancel(self.turnOnCamera)
-            self.turnOnCamera = None
-        check1, image1 = self.webcam.read()
-        cv2.imwrite(filename="images/image_object_" + str(id(check1)) + '.jpg', img=image1)
+            self.webcam.release()
+            self.off = 1
+
+
+    def getImage(self):
+        # stop the streaming
+        # self.window.after_cancel(self.turnOnCamera)
+        # get a photo and save
+        # check1, image1 = self.webcam.read()
+        ts = datetime.datetime.now()  # grab the current timestamp
+        filename = "{}.jpg".format(ts.strftime("%Y-%m-%d_%H-%M-%S"))
+        cv2.imwrite(filename=filename, img=self.current_image) # "images/image_object_" + str(id(check1)) + '.jpg'
+        # check, that streaming is off
+        # self.off = 1
 
 
     def onClose(self):
